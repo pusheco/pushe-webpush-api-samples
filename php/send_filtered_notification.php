61a994e022eb33f116bb33fddc0d923e1667e862
srwah <serveh.mamghaderi@gmail.com>
2021-08-09 00:33:13 +0430

    Update and rename send_filtered_notification_with_device_id.php to send_filtered_notification.php

--- php/send_filtered_notification.php
+++ php/send_filtered_notification.php
@@ -1,10 +1,9 @@
 <?php
 
-// More info about device_id:
-//    (web): https://pushe.co/docs/webpush/#unique_id
-
+// Obtain token -> https://docs.pushe.co/docs/web-api/authentication
 $TOKEN = "YOUR_TOKEN";
 
+// Webpush doc -> https://docs.pushe.co/docs/web-api/filtered-notification
 $data = array(
     "app_ids" => ["YOUR_APP_ID",],
     "data" => array(
@@ -14,8 +13,7 @@ $data = array(
     "filters" => array(
         "device_id" => ["DEVICE_ID_1","DEVICE_ID_2"]
     ),
-    // additional keywords -> https://pushe.co/docs/api/#api_send_advance_notification
-);
+    // additional keywords -> https://docs.pushe.co/docs/web-api/notification-keys
 
 $ch = curl_init("https://api.pushe.co/v2/messaging/notifications/web");
 
